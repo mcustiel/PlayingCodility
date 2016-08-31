07f53d7f4271f012a77135ac46c246fa9cf714b2
Mariano Custiel <mcustiel@goodgamestudios.com>
2016-08-31 16:49:40 +0200

    Checking

--- src/main/java/org/mcustiel/codility/CodilityGammaChallenge.java
+++ src/main/java/org/mcustiel/codility/CodilityGammaChallenge.java
@@ -6,48 +6,36 @@ public class CodilityGammaChallenge {
 	public int solution(String S) {
 		int palindroms = 0, control = 0, head = 1, tail = S.length()-1;
 
-		//palindroms += isPalindrom(S) ? S.length() / 2 : 0;
 
-		System.out.println("=====");
-		System.out.println("Analyzing: " + S);
-		System.out.println("=====");
+	    palindroms = getPalindromsCount(S);
 
-		if (S.length() <= 3) {
-			return getPalindromsCount(S);
-		}
-
-		while (head <= tail) {
-			if (control % 2 == 0) {
-				palindroms += getPalindromsCount(S.substring(head, tail))
-					+ getPalindromsCount(S.substring(0, head))
-					+ getPalindromsCount(S.substring(head, S.length()));
-				head++;
-			} else {
-				palindroms += getPalindromsCount(S.substring(head, tail))
-						+ getPalindromsCount(S.substring(0, tail))
-						+ getPalindromsCount(S.substring(tail, S.length()));
-				tail--;
+		if (S.length() > 3) {
+			while (head <= tail) {
+				if (control % 2 == 0) {
+					palindroms += getPalindromsCount(S.substring(head, tail))
+						+ getPalindromsCount(S.substring(0, head))
+						+ getPalindromsCount(S.substring(head, S.length()))
+						+ getPalindromsCount(S.substring(head, S.length()/2+1));
+					head++;
+				} else {
+					palindroms += getPalindromsCount(S.substring(head, tail))
+							+ getPalindromsCount(S.substring(0, tail))
+							+ getPalindromsCount(S.substring(tail, S.length()))
+							+ getPalindromsCount(S.substring(S.length()/2+1, tail));
+					tail--;
+				}
+				control++;
 			}
-			control++;
-			System.err.println("---");
 		}
-/*
-		for (int i = 0; i < S.length()/2; i++) {
-			palindroms += getPalindromsCount(S.substring(i, S.length() - 1 - i))
-				+ getPalindromsCount(S.substring(i, (S.length() - 1) / 2))
-				+ getPalindromsCount(S.substring((S.length() - 1) / 2, S.length() - 1 - i));
-		}
-*/
-		return palindroms + getPalindromsCount(S);
+		return palindroms;
 	}
 
 	private int getPalindromsCount(String string)
 	{
-		return isPalindrom(string) ? 1 : 0;
+		return isPalindrom(string) ? string.length()/2: 0;
 	}
 
 	private boolean isPalindrom(String string) {
-		System.out.println(string);
 		if (string.length() <= 1) {
 			return false;
 		}
