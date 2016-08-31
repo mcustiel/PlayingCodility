a8e6f2c840f4626febbb6380775a83adde05225e
Mariano Custiel <mcustiel@goodgamestudios.com>
2016-08-31 10:17:54 +0200

    Now it's all broken but I think it's closer

--- src/main/java/org/mcustiel/codility/CodilityGammaChallenge.java
+++ src/main/java/org/mcustiel/codility/CodilityGammaChallenge.java
@@ -4,24 +4,58 @@ package org.mcustiel.codility;
 
 public class CodilityGammaChallenge {
 	public int solution(String S) {
-		int palindroms = 0;
+		int palindroms = 0, control = 0, head = 1, tail = S.length()-1;
 
-		palindroms += isPalindrom(S) ? S.length() / 2 : 0;
+		//palindroms += isPalindrom(S) ? S.length() / 2 : 0;
 
-		System.out.println("----");
+		System.out.println("=====");
+		System.out.println("Analyzing: " + S);
+		System.out.println("=====");
 
-		/*for (int i = 1; i < S.length(); i++) {
+		if (S.length() <= 3) {
+			return getPalindromsCount(S);
+		}
 
-		}*/
+		while (head < tail) {
+			palindroms += getPalindromsCount(S.substring(head, tail))
+					+ getPalindromsCount(S.substring(0, tail))
+					+ getPalindromsCount(S.substring(0, head))
+					+ getPalindromsCount(S.substring(head, S.length()))
+					+ getPalindromsCount(S.substring(tail, S.length()));
 
-		return palindroms;
+			if (control % 2 == 0) {
+				head++;
+			} else {
+				tail--;
+			}
+			control++;
+			System.err.println("---");
+		}
+/*
+		for (int i = 0; i < S.length()/2; i++) {
+			palindroms += getPalindromsCount(S.substring(i, S.length() - 1 - i))
+				+ getPalindromsCount(S.substring(i, (S.length() - 1) / 2))
+				+ getPalindromsCount(S.substring((S.length() - 1) / 2, S.length() - 1 - i));
+		}
+*/
+		return palindroms + getPalindromsCount(S);
+	}
+
+	private int getPalindromsCount(String string)
+	{
+		return isPalindrom(string) ? string.length() / 2 : 0;
 	}
 
 	private boolean isPalindrom(String string) {
 		System.out.println(string);
+		if (string.length() <= 1) {
+			return false;
+		}
 
 		return string.charAt(0) == string.charAt(string.length() - 1)
 				&& (string.length() <= 3 || isPalindrom(
 						string.substring(1, string.length() - 1)));
 	}
+
+
 }
