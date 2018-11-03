df54b229343bfb78269425ba2251408ead4ca542
Mariano Custiel <jmcustiel@gmail.com>
2018-11-03 14:02:03 +0100

    Small improvements. I suspect it should be O(n), but how?

--- src/main/java/org/mcustiel/codility/CodilityGammaChallenge.java
+++ src/main/java/org/mcustiel/codility/CodilityGammaChallenge.java
@@ -5,25 +5,23 @@ package org.mcustiel.codility;
 public class CodilityGammaChallenge {
 
 	public int solution(String S) {
-		int length = S.length() - 1;
+		int length = S.length();
+		int end = length - 1;
 		int palindroms = 0;
-		for (int i = 0; i < length; i++) {
-			palindroms += count(S, i, i + 1);
-			palindroms += count(S, i, i + 2);
-			if (palindroms >= 100000000) {
+		for (int i = 0; i < end; i++) {
+			palindroms += count(S, i, i + 1, length, palindroms);
+			palindroms += count(S, i, i + 2, length, palindroms);
+			if (palindroms > 100000000) {
 				return -1;
 			}
 		}
 		return palindroms;
 	}
 
-	private int count(String S, int start, int end) {
-		if (start < 0 || end >= S.length()) {
+	private int count(String S, int start, int end, int length, int palindroms) {
+		if (start < 0 || end - length == 0 || S.charAt(start) != S.charAt(end) || palindroms > 100000000) {
 			return 0;
 		}
-		if (S.charAt(start) == S.charAt(end)) {
-			return 1 + count(S, start - 1, end + 1);
-		}
-		return 0;
+		return 1 + count(S, start - 1, end + 1, length, palindroms+1);
 	}
 }
