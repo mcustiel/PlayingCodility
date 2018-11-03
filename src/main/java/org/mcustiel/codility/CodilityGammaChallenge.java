3e66ba489226c66304bf4878b94fa1f6d1d707fa
Mariano Custiel <jmcustiel@gmail.com>
2018-11-03 19:23:21 +0100

    Renamed variable

--- src/main/java/org/mcustiel/codility/CodilityGammaChallenge.java
+++ src/main/java/org/mcustiel/codility/CodilityGammaChallenge.java
@@ -5,42 +5,39 @@ package org.mcustiel.codility;
 public class CodilityGammaChallenge {
 
 	public int solution(String S) {
-		int length = S.length();
-		int end = length - 2;
-		int palindroms = 0;
+		int length = S.length(), end = length - 2,palindromes = 0;
 		int i = end, j;
-		int[][] check = new int[length][length/2];
-		int[] items = new int[length/2];
+		int[][] check = new int[length][length];
+		int[] items = new int[length];
 
 		if (length > 1) {
-
 			char[] characters = S.toCharArray();
 
 			if (characters[i] == characters[i + 1]) {
 				if (i > 0) {
 					check[i - 1][items[i - 1]++] = 3;
 				}
-				palindroms++;
+				palindromes++;
 			}
 			end--;
 
-			for (i = end; i >= 0 && palindroms <= 100000000; i--) {
+			for (i = end; i >= 0 && palindromes <= 100000000; i--) {
 				if (characters[i] == characters[i + 1]) {
 					if (i > 0) {
 						check[i - 1][items[i - 1]++] = 3;
 					}
-					palindroms++;
+					palindromes++;
 				}
 				if (characters[i] == characters[i + 2]) {
 					if (i > 0) {
 						check[i - 1][items[i - 1]++] = 4;
 					}
-					palindroms++;
+					palindromes++;
 				}
 
 				for (j = 0; j < items[i]; j++) {
 					if (i + check[i][j] < length && characters[i] == characters[i + check[i][j]]) {
-						palindroms++;
+						palindromes++;
 						if (i > 0) {
 							check[i - 1][items[i - 1]] = check[i][j] + 2;
 							items[i - 1]++;
@@ -48,10 +45,10 @@ public class CodilityGammaChallenge {
 					}
 				}
 			}
-			if (palindroms > 100000000) {
+			if (palindromes > 100000000) {
 				return -1;
 			}
 		}
-		return palindroms;
+		return palindromes;
 	}
 }
