7520dd1165dd74a9aeb6a83adefd3097fde2dd9b
Mariano Custiel <jmcustiel@gmail.com>
2018-11-04 16:01:13 +0100

    Faster, but still not enough.

--- src/main/java/org/mcustiel/codility/CodilityGammaChallenge.java
+++ src/main/java/org/mcustiel/codility/CodilityGammaChallenge.java
@@ -4,31 +4,40 @@ package org.mcustiel.codility;
 
 public class CodilityGammaChallenge {
 
+	/**
+	 * @param S
+	 * @return
+	 */
 	public int solution(String S) {
-		int length = S.length(), end = length - 2, palindromes = 0;
-		int i = end, j;
-		int[][] check = new int[length][length];
-		int[] items = new int[length];
+		int stringLength = S.length(), palindromes = 0;
+		int i, j;
+		int[][] palindromesIndexes = new int[2][stringLength];
+		int[] palindromesToCheck = new int[2];
+		int curPos, nextPos;
 
-		if (length > 1) {
+		if (stringLength > 1) {
 			char[] characters = S.toCharArray();
 
-			for (i = length-1; i > 0 && palindromes <= 100000000; i--) {
-				for (j = 0; j < items[i]; j++) {
-					if (i + check[i][j] < length && characters[i] == characters[i + check[i][j]]) {
+			for (i = stringLength - 1; i > 0; i--) {
+				curPos = i % 2;
+				nextPos = (i - 1) % 2;
+				palindromesToCheck[nextPos] = 0;
+
+				for (j = 0; j < palindromesToCheck[curPos]; j++) {
+					if (palindromesIndexes[curPos][j] < stringLength
+							&& characters[i] == characters[palindromesIndexes[curPos][j]]) {
 						palindromes++;
-						if (i > 0) {
-							check[i - 1][items[i - 1]] = check[i][j] + 2;
-							items[i - 1]++;
-						}
+						palindromesIndexes[nextPos][palindromesToCheck[nextPos]] = palindromesIndexes[curPos][j] + 1;
+						palindromesToCheck[nextPos]++;
 					}
 				}
-				check[i - 1][items[i - 1]++] = 1;
-				check[i - 1][items[i - 1]++] = 2;
+				palindromesIndexes[nextPos][palindromesToCheck[nextPos]++] = i;
+				palindromesIndexes[nextPos][palindromesToCheck[nextPos]++] = i + 1;
 			}
 			if (i == 0) {
-				for (j = 0; j < items[i]; j++) {
-					if (i + check[i][j] < length && characters[i] == characters[i + check[i][j]]) {
+				for (j = 0; j < palindromesToCheck[i]; j++) {
+					if (palindromesIndexes[i][j] < stringLength
+							&& characters[i] == characters[palindromesIndexes[i][j]]) {
 						palindromes++;
 					}
 				}
--- src/test/java/org/mcustiel/codility/CodilityGammaChallengeTest.java
+++ src/test/java/org/mcustiel/codility/CodilityGammaChallengeTest.java
@@ -9,70 +9,79 @@ import org.junit.Test;
 // WORK IN PROGRESS
 
 public class CodilityGammaChallengeTest {
-    private CodilityGammaChallenge testee;
+	private CodilityGammaChallenge testee;
 
-    @Before
-    public void init() {
-    	testee = new CodilityGammaChallenge();
-    }
+	@Before
+	public void init() {
+		testee = new CodilityGammaChallenge();
+	}
 
-    @Test
-    public void testOne() {
-    	assertThat(testee.solution("abccba"), is(3));
-    }
+	@Test
+	public void testOne() {
+		assertThat(testee.solution("abccba"), is(3));
+	}
 
-    @Test
-    public void testTwo() {
-    	assertThat(testee.solution("abcba"), is(2));
-    }
+	@Test
+	public void testTwo() {
+		assertThat(testee.solution("abcba"), is(2));
+	}
 
-    @Test
-    public void testThree() {
-    	assertThat(testee.solution("baababa"), is(6));
-    }
+	@Test
+	public void testThree() {
+		assertThat(testee.solution("baababa"), is(6));
+	}
 
-    @Test
-    public void testFour() {
-    	assertThat(testee.solution("caabbbaa"), is(7));
-    }
+	@Test
+	public void testFour() {
+		assertThat(testee.solution("caabbbaa"), is(7));
+	}
 
-    @Test
-    public void testFive() {
-    	assertThat(testee.solution(""), is(0));
-    }
-    @Test
-    public void testNein() {
-    	assertThat(testee.solution("a"), is(0));
-    }
+	@Test
+	public void testFive() {
+		assertThat(testee.solution(""), is(0));
+	}
 
-    @Test
-    public void testSix() {
-    	assertThat(testee.solution("ab"), is(0));
-    }
+	@Test
+	public void testNein() {
+		assertThat(testee.solution("a"), is(0));
+	}
 
-    @Test
-    public void testSeven() {
-    	assertThat(testee.solution("bb"), is(1));
-    }
+	@Test
+	public void testSix() {
+		assertThat(testee.solution("ab"), is(0));
+	}
 
-    @Test
-    public void testEight() {
-    	assertThat(testee.solution("bab"), is(1));
-    }
+	@Test
+	public void testSeven() {
+		assertThat(testee.solution("bb"), is(1));
+	}
 
-    @Test
-    public void testTen() {
-    	assertThat(testee.solution(
-    			new String(new char[1000]).replace("\0", "a") +
-    			new String(new char[1000]).replace("\0", "b") +
-    			new String(new char[1000]).replace("\0", "c")),
-			is(1498500));
-    }
+	@Test
+	public void testEight() {
+		assertThat(testee.solution("bab"), is(1));
+	}
 
-    @Test
-    public void testEleven() {
-    	assertThat(testee.solution(
-    			new String(new char[20000]).replace("\0", "a")),
-			is(-1));
-    }
+	@Test
+	public void testTen() {
+		assertThat(testee.solution(new String(new char[1000]).replace("\0", "a")
+				+ new String(new char[1000]).replace("\0", "b") + new String(new char[1000]).replace("\0", "c")),
+				is(1498500));
+	}
+
+	@Test
+	public void testEleven() {
+		assertThat(testee.solution(new String(new char[20000]).replace("\0", "a")), is(-1));
+	}
+
+	@Test
+	public void testTwelve() {
+		assertThat(testee.solution(new String(new char[10000]).replace("\0", "ab")), is(99990000));
+	}
+
+	@Test
+	public void testThirteen() {
+		assertThat(testee.solution(new String(new char[2000]).replace("\0", "b")
+				+ new String(new char[2000]).replace("\0", "a") + new String(new char[2000]).replace("\0", "b")
+				+ new String(new char[2000]).replace("\0", "a")), is(8000000));
+	}
 }
