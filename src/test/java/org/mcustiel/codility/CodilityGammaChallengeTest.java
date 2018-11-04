6d3663c03acb08e64799ec55027023a9edd111a7
Mariano Custiel <jmcustiel@gmail.com>
2018-11-04 23:04:30 +0100

    I consider it finished. Because it shows much faster times in my laptop that it does in codility.

--- src/test/java/org/mcustiel/codility/CodilityGammaChallengeTest.java
+++ src/test/java/org/mcustiel/codility/CodilityGammaChallengeTest.java
@@ -6,8 +6,6 @@ import static org.junit.Assert.assertThat;
 import org.junit.Before;
 import org.junit.Test;
 
-// WORK IN PROGRESS
-
 public class CodilityGammaChallengeTest {
 	private CodilityGammaChallenge testee;
 
